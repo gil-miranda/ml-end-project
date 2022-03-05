892776ecf6ab15ce90f6b97f1d008acae1d32fc8
Gil Miranda <gilsmneto@gmail.com>
2022-03-05 09:04:48 -0300

    feat:  download the dataset with makefile command

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,48 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import subprocess
+from kaggle.api import KaggleApi
+from zipfile import ZipFile
+import os
+import yaml
 
-
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+CONFIG_FILE = 'config.yaml'
+def download_dataset():
+    """ Download and extract the Kaggle MNIST dataset.
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    with open(CONFIG_FILE) as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
 
+    competition = config['data']['kaggle']['competition']
+    files = config['data']['kaggle']['files']
+    output_dir = config["data"]["raw_folder"]
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    api = KaggleApi()
+    api.authenticate()
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    for d in files.values():
+        subprocess.run(
+            [
+                "kaggle",
+                "competitions",
+                "download",
+                f"{competition}",
+                "-f"
+                f"{d}",
+                "--path",
+                f"{output_dir}",
+            ],
+            check=True,
+        )
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+        with ZipFile(f'{output_dir}/{d}.zip', 'r') as zipObj:
+            zipObj.extractall(f'{output_dir}')
 
-    main()
+        try:
+            os.remove(f'{output_dir}/{d}.zip')
+        except exc:
+            print(exc)
+
+if __name__ == "__main__":
+    download_dataset()
